d9767b0b90d11ddc863ac1739eaa78e144021276
Sergiu Stascu <sergiustascu@gmail.com>
2023-03-10 13:53:52 +0200

    5. Find Second largest element in an array. 6. Find the sum of digits of a number read from the keyboard. 7. Convert Binary 10011010 to Decimal number system.

--- CSharpTrainingConsoleApp/Ex1_and_ex3.cs
+++ CSharpTrainingConsoleApp/Ex1_and_ex3.cs
@@ -8,16 +8,35 @@ namespace CSharpTrainingConsoleApp
 {
     public class Number
     {
-        public int myNumber;
-
+        private int decRepresentation;
+        private String binaryRepresentation;
+       
         public Number(int myNumber)
         {
-            this.myNumber = myNumber;
+            this.decRepresentation = myNumber;
+            this.binaryRepresentation = convertIntBinaryFromDec();
+        }
+
+        public Number(String binaryRepresentation)
+        {
+            this.binaryRepresentation = binaryRepresentation;
+            this.decRepresentation = convertInDecFromBinary();
+        }
+
+
+        public int getDecRepresenatation()
+        {
+            return decRepresentation;
+        }
+
+        public String getBinaryRepresentation()
+        {
+            return binaryRepresentation;    
         }
 
         public bool isDivisibleBy(int divisor) 
         {
-            return myNumber % divisor == 0; 
+            return decRepresentation % divisor == 0; 
         }
 
         public bool isDivisibleBy7and11()
@@ -28,17 +47,71 @@ namespace CSharpTrainingConsoleApp
         public bool isPrime()
         {
             int index;
-            if (myNumber <= 1)
+            if (decRepresentation <= 1)
                 return false;
-            if (myNumber == 2)
+            if (decRepresentation == 2)
                 return true;
 
-            for (index = 2; index <= ((int)Math.Sqrt(myNumber))+1; index++)
+            for (index = 2; index <= ((int)Math.Sqrt(decRepresentation))+1; index++)
             {
-                if(myNumber%index== 0) return false;
+                if(decRepresentation%index== 0) return false;
             }
             return true;
-            
+        }
+
+        public void setAstPrimeByIndex(int primeIndex)
+        {
+            int actualPrimeIndex = 0;
+            while (actualPrimeIndex != primeIndex)
+            {
+                decRepresentation++;
+                if (isPrime())
+                {
+                    actualPrimeIndex++;
+                }
+            }
+        }
+
+        public int getSumOfDigits()
+        {
+            int sum = 0;
+            int aux = decRepresentation;
+            while (aux != 0)
+            {
+                sum += aux % 10;
+                aux = (int)aux / 10;
+            }
+            return sum;
+        }
+
+
+        public int convertInDecFromBinary()
+        {
+            double poz = 0;
+            int decValue = 0;
+
+            char[] auxiliatCharArray = binaryRepresentation.ToCharArray();
+            Array.Reverse(auxiliatCharArray);
+            foreach(char c in auxiliatCharArray)
+            {
+                decValue += ((int)c - 0x30)* (int)Math.Pow(2.0,poz++);
+            }
+
+            return decValue;
+        }
+
+        public string convertIntBinaryFromDec()
+        {
+            string auxCharArray = "";
+            int aux = decRepresentation;
+            while (aux != 0)
+            {
+                
+                auxCharArray += (aux%2).ToString();
+                aux = aux / 2;
+            }
+            auxCharArray.Reverse();
+            return auxCharArray;
         }
     }
 }
--- CSharpTrainingConsoleApp/Program.cs
+++ CSharpTrainingConsoleApp/Program.cs
@@ -17,6 +17,7 @@ for(; ; )
     Console.WriteLine("Choose exercise, from 1 to 7, 100 to exit:");
     int exerciceNumber= int.Parse( Console.ReadLine() );
     Console.WriteLine("Exercice "+ exerciceNumber);
+    Console.WriteLine();
 
     switch (exerciceNumber)
     {
@@ -35,24 +36,12 @@ for(; ; )
         case 3:
             Console.WriteLine("47th prime number is :");
             Number number1 = new Number(0);
-            int index = 0;
-            while (index!=47)
-            {
-                number1.myNumber++;
-                if (number1.isPrime())
-                {
-                    index++;
-                }
-            }
-            Console.WriteLine(number1.myNumber.ToString());
+            number1.setAstPrimeByIndex(47);
+            Console.WriteLine(number1.getDecRepresenatation().ToString());
             break;
         case 4:
             Fibonaci fibonaci = new Fibonaci();
-            int elementIndex;
-            for(elementIndex = 3; elementIndex <=10; elementIndex++)
-            {
-                fibonaci.addNextElement();
-            }
+            fibonaci.addNextNElements(8);
             fibonaci.printFibonaciList("First 10 elements from Fibonaci list:");
 
             Console.WriteLine("Sum of even elements under 1000");
@@ -62,10 +51,29 @@ for(; ; )
             List<int> evenListOfElements = fibonaci2.getEvenElements();
             printList(evenListOfElements, "List of even elements under 1000");
             break;
-        case 100: return;
+        case 5:
+            NumberArray numberArray = new NumberArray();
+            numberArray.generateListFromRandomNElements(10);
+            printList(numberArray.list, "Array of random numbers");
+            Console.WriteLine("Second largest is: ");
+            Console.WriteLine(numberArray.getSecondLargestElement());
+            break;
+        case 6:
+            Console.WriteLine("Give a number ti calculate sum of digits:");
+            Number number3 = new Number(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Sum is: "+ number3.getSumOfDigits());
+            break;
+        case 7:
+            Console.WriteLine("Give a binar number to transfort it in decimal:");
+            Number number4 = new Number(Console.ReadLine());
+            Console.WriteLine("Decimal value is: "+ number4.getDecRepresenatation());
+
+            break
+;        case 100: return;
 
         default: Console.WriteLine("Invalid execise number!!!"); break;
-     
+
+            Console.WriteLine();
 
     }
     
